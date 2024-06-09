b072321a8589e30a55b75e3684eeeedfbbff43c5
Thales Davi de Souza <thalesdavisoul@gmail.com>
2024-06-08 22:24:36 -0300

    Adding task chamada

---
+++ src/Set/Lista de Chamada/chamada.cpp
@@ -0,0 +1,39 @@
+/**
+ * O desafio aqui é completar essa tarefa
+ * utilizando set. Outras estruturas seriam
+ * mais eficientes, como o map.
+ */
+
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main(){
+    set<string> nomes;
+    int alunoSorteado;
+    int numAlunos;
+
+    cin >> numAlunos >> alunoSorteado;
+
+    for(int i = 0; i < numAlunos; i++){
+        string aluno;
+        cin >> aluno;
+        nomes.insert(aluno);
+    }
+
+    int i = 1;
+    for(const auto&nome : nomes){
+        if(i == alunoSorteado){
+            cout << nome;
+            break;
+        }
+        else{
+            i++;
+        }
+    }
+
+
+    return 0;
+}
